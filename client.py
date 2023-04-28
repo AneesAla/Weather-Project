bc1d682a1d54eb6737e67b8301008b0a48c34926
Abdullah Shafique <abdshafique@hotmail.com>
2023-04-28 15:04:55 -0400

    Add OpenWeatherAPI and Nominatim usage

--- client.py
+++ client.py
@@ -1,25 +1,23 @@
 import socket
+import json
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 9123))
-msg = s.recv(1024) # receive a message of 1024 char buffer
+msg = s.recv(1024)
 print(msg.decode("utf-8"))
-state = ""
 
-# prompt the user to input where and when they want the weather for
-country = input("Enter the name of a country (Please spell out the Full name of the country): ")
-if country == "United States" or country ==  "united states":
-    state = input("Please input the state: ")
-city = input("Please enter the city you would like to get the weather for: ")
-startDate = input("Please input the start date (mm/dd/yyyy): ")
-endDate = input("Please enter the end date (mm/dd/yyyy): ")
+state = input("Please input the state: ")
+city = input("Please enter the city: ")
 
-# Combine the country, state (if applicable), and city into a single string
-location = f"{country},{state},{city}" if state else f"{country},{city}"
+location = f"{city},{state}"
 
-# Combine the start and end dates into a single string
-date_range = f"{startDate},{endDate}"
-
-# Send the location and date range to the server
-message = f"{location};{date_range}".encode("utf-8")
+message = f"{location}".encode("utf-8")
 s.send(message)
+
+msg = s.recv(1024)
+data = json.loads(msg.decode("utf-8"))
+
+print("Temp: " + str(data["temp"]))
+print("Feels Like: " + str(data["feels_like"]))
+print("Sea Level: " + str(data["sea_level"]))
+print("Humidity: " + str(data["humidity"]))
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import requests
+import json
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 9123))
@@ -8,20 +10,41 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome Weather App By Anees Alawmleh!\nThis app allows you to find the weather for a city of your choice!", "utf-8")) # sending info the client
+    clientsocket.send(
+        bytes(
+            "Welcome Weather/Event App By Anees Alawmleh!\nThis app allows you to find the weather for a city of your choice!",
+            "utf-8",
+        )
+    )
 
-    # Receive the location and date range from the client
     message = clientsocket.recv(1024)
-    location, date_range = message.decode("utf-8").split(";")
+    location = message.decode("utf-8")
 
-    # Split the location into separate parts
     location_parts = location.split(",")
-    country = location_parts[0]
-    state = location_parts[1] if len(location_parts) > 2 else ""
-    city = location_parts[-1]
+    city = location_parts[0]
+    state = location_parts[1]
 
-    # Split the date range into separate start and end dates
-    start_date, end_date = date_range.split(",")
+    lat = 0.0
+    lon = 0.0
 
+    nominatimAPI = (
+        f"https://nominatim.openstreetmap.org/search/{city} {state}?format=json&limit=1"
+    )
+    nominatimAPIResponse = requests.get(nominatimAPI)
 
-    print(country, state, city, start_date, end_date)
+    if nominatimAPIResponse.status_code == 200:
+        nominatimData = nominatimAPIResponse.json()
+        lat = nominatimData[0]["lat"]
+        lon = nominatimData[0]["lon"]
+
+        openweathermapAPI = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid=8cf1d48bb4fb4a8548e95957e464b010"
+        openweathermapAPIResponse = requests.get(openweathermapAPI)
+
+        if openweathermapAPIResponse.status_code == 200:
+            openweathermapData = openweathermapAPIResponse.json()
+            clientsocket.send(
+                bytes(
+                    json.dumps(openweathermapData["list"][0]["main"]),
+                    "utf-8",
+                )
+            )
